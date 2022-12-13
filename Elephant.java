55bb7324b3d84834dff6b7db4b84a40a731c976d
groobob <349578260@gapps.yrdsb.ca>
2022-12-13 17:58:36 -0500

    Completed up to 2.6

--- Elephant.java
+++ Elephant.java
@@ -8,18 +8,34 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Elephant extends Actor
 {
-    /**
-     * Act - do whatever the elephant wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    GreenfootSound elephantSound = new GreenfootSound("elephantcub.mp3");
+    GreenfootImage[] idle = new GreenfootImage[8];
+    
+    public Elephant()
+    {
+        for(int i = 0; i < idle.length; i++)
+        {
+            idle[i] = new GreenfootImage("images/elephant_idle/idle" + i + ".png");
+        }
+        setImage(idle[0]);
+    }
+    int imageIndex = 0;
+    public void animateElephant()
+    {
+        setImage(idle[imageIndex]);
+        imageIndex = (imageIndex + 1) % idle.length;
+    }
     public void act()
     {
         // Add your action code here.
-        if(Greenfoot.isKeyDown("left")) move(-1);
-        else if(Greenfoot.isKeyDown("right")) move(1);
+        if(Greenfoot.isKeyDown("left")) move(-3);
+        else if(Greenfoot.isKeyDown("right")) move(3);
         
         // Remove an apple after it has been eating
         eat();
+        
+        // Animate the elephant
+        animateElephant();
     }
     
     //Code for the elephant to eat an apple
@@ -31,6 +47,7 @@ public class Elephant extends Actor
             MyWorld world = (MyWorld) getWorld();
             world.createApple();
             world.increaseScore();
+            elephantSound.play();
         }
     }
 }
